a937e64642617ba3d0562b4f50bad496f5e1697b
Danny Padilla <dpadilla010@gmail.com>
2018-03-30 16:27:19 -0700

    add cmd line options

--- roi.py
+++ roi.py
@@ -1,5 +1,18 @@
+'''
+This program provides something.... 
+* 1. Do 
+* 2. Then that
+
+Usage:
+ roi.py [<some_args>, here]
+
+Output:
+ Stores an image... somewhere
+'''
 import cv2
 import numpy as np
+import os, sys
+from subprocess import check_output
 
 # Drawing crop section
 drawing = False
@@ -9,8 +22,10 @@ coordinate = []
 ix = -1
 iy = -1
 
+
 def redraw(img):
     pass
+
 
 # Function to set cropped region
 def draw(event, x, y, flags, param):
@@ -23,17 +38,34 @@ def draw(event, x, y, flags, param):
         draw = False
         cv2.rectangle(img, (x, y), (ix,iy), (0, 255, 0), 1)
 
-img = np.zeros( (512, 512, 3), np.uint8)
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw)
 
-#img2 = img.copy
+# ***************************************************************** #
 
-while(1):
-    cv2.imshow('image', img)
-    key = cv2.waitKey(1) & 0xFF
+if __name__ == "__main__":
 
-    # Wait for ESC Key to exit
-    if key == 27:
-        break
-cv2.destroyAllWindows()
+    clear_screen_cmd = check_output(["clear", "."]).decode("utf8") # create clear cmd
+    print(clear_screen_cmd) # all this does is clear the terminal screen for doc ( ctrl + l)
+
+    print(__doc__) # prints doc info to screen
+
+    try:
+        fn = sys.argv[1] # cmd line arg for img_path or block_size?
+    except:
+        fn = "default/img/path"
+
+    #print(fn) # should error check if argv is passed and valid
+
+    img = np.zeros( (512, 512, 3), np.uint8)
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', draw)
+
+    #img2 = img.copy
+
+    while(1):
+        cv2.imshow('image', img)
+        key = cv2.waitKey(1) & 0xFF
+
+        # Wait for ESC Key to exit
+        if key == 27:
+            break
+    cv2.destroyAllWindows()
